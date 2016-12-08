92d74ac80118e26c0ea7b599254a259182dd09a9
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-08 15:04:03 +0100

    .

--- run_test.py
+++ run_test.py
@@ -1,3 +1,22 @@
 import utils
+import argparse
 
-print utils.test2(C1=3, gamma=0.01)
+############
+#   MAIN   #
+############
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Predict results from a file')
+    parser.add_argument('-serie', type=str, required=False, help='Serie (serie-a, bundesliga, ...)')
+    parser.add_argument('-C', type=str, required=False, help='C parameter')
+    parser.add_argument('-gamma', type=str, required=False, help='Gamma parameter')
+    args = parser.parse_args()
+    if args.gamma is None:
+        gamma = 0.01
+    else:
+        gamma = args.gamma
+    if args.C is None:
+        C = 0.01
+    else:
+        C = args.C
+
+    print utils.test2(C, gamma)

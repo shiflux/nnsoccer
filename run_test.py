7e7a6d966cbab531c391699f8ebf7f5688c627fd
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-08 16:44:34 +0100

    .

--- run_test.py
+++ run_test.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     parser.add_argument('-serie', type=str, required=False, help='Serie (serie-a, bundesliga, ...)')
     parser.add_argument('-C', type=str, required=False, help='C parameter')
     parser.add_argument('-gamma', type=str, required=False, help='Gamma parameter')
-    parser.add_argument('-gamma', type=str, required=False, help='Gamma parameter')
     parser.add_argument('-last', required=False, help='2015/2016 only')
     args = parser.parse_args()
     if args.gamma is None:

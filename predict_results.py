7c268448d9a92bf2e80eb4e9bf599c5c2379aaee
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-09 22:11:54 +0100

    .

--- predict_results.py
+++ predict_results.py
@@ -18,8 +18,7 @@ if __name__ == "__main__":
             stocks = myFile.read().splitlines()
 
             for line in stocks:
-                print line
-                game = line.split(',')
+                game = line.split(';')
                 if args.v:
                     print game
                 if len(game) == 2:

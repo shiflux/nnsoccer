c4e0fb8cdafcc53acea70d0d1f19a09386929806
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-09 16:48:54 +0100

    .

--- predict_results.py
+++ predict_results.py
@@ -14,29 +14,26 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.filename is not None and args.serie is not None:
         my_game_list = []
-        try:
-            myFile = open(args.filename, 'r')
-        except Exception as ex:
-            print ("Error opening file: ", ex.message)
+        with open(args.filename) as myFile:
+            stocks = myFile.read().splitlines()
 
-        for line in myFile:
-            line = line.strip("/r/n")
-            game = line.split(',')
-            if args.v:
-                print game
-            if len(game) == 2:
-                my_game_list.append((game[0].strip(), game[1].strip()))
-        if args.gamma is None:
-            gamma = 0.01
-        else:
-            gamma = args.gamma
-        if args.C is None:
-            C = 0.01
-        else:
-            C = args.C
+            for line in stocks:
+                game = line.split(',')
+                if args.v:
+                    print game
+                if len(game) == 2:
+                    my_game_list.append((game[0].strip(), game[1].strip()))
+            if args.gamma is None:
+                gamma = 0.01
+            else:
+                gamma = args.gamma
+            if args.C is None:
+                C = 0.01
+            else:
+                C = args.C
 
-        if len(my_game_list) > 0:
-            print (utils.predict_test2(my_game_list, serie=args.serie, C1= 3, gamma=0.01))
-        else:
-            print ("Error not enough games")
+            if len(my_game_list) > 0:
+                print (utils.predict_test2(my_game_list, serie=args.serie, C1= 3, gamma=0.01))
+            else:
+                print ("Error not enough games")
 

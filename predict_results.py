e5159d27e74777446953410e4849a1a1ec230824
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-09 22:19:57 +0100

    .

--- predict_results.py
+++ predict_results.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
             stocks = myFile.read().splitlines()
 
             for line in stocks:
-                game = line.split(';')
+                game = line.encode('UTF-8').split(';')
                 if args.v:
                     print game
                 if len(game) == 2:

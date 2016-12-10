786ceb00c4acc1e9db92154c144c142df3846238
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-10 12:11:14 +0100

    .

--- utils.py
+++ utils.py
@@ -214,7 +214,7 @@ def fit(X, y, X1, y1, C1=2, gamma=0.2):
         # if predicted_prob[x][predicted[x]] > threshold:
         flag1 = numpy.absolute(predicted_prob[x][predicted[x]] - predicted_prob[x][(predicted[x]+1)%3])
         flag2 = numpy.absolute(predicted_prob[x][predicted[x]]- predicted_prob[x][(predicted[x]+2)%3])
-        if max_threshold > flag1 > threshold and max_threshold > flag2 > threshold:
+        if max_threshold > flag1 >= threshold and max_threshold > flag2 >= threshold:
             if predicted[x] == y1[x]:
                 temp_res.append(1)
             else:

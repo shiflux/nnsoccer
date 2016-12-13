a207f93d82e9d48634f1b2685cc0047fce1c9a44
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-13 22:24:06 +0100

    .

--- utils.py
+++ utils.py
@@ -219,11 +219,11 @@ def fit(X, y, X1, y1, C1=2, gamma=0.2):
         if predicted[x] == 1:
             if predicted_prob[x][predicted[x]] >= 0.3 and flag1 <= 0.45 and flag2 <= 0.45:
                 temp_res.append(1)
-            elif max_threshold > flag1 >= threshold and max_threshold > flag2 >= threshold:
-                if predicted[x] == y1[x]:
-                    temp_res.append(1)
-                else:
-                    temp_res.append(0)
+        elif max_threshold > flag1 >= threshold and max_threshold > flag2 >= threshold:
+            if predicted[x] == y1[x]:
+                temp_res.append(1)
+            else:
+                temp_res.append(0)
     print(len(temp_res))
     print(numpy.mean(temp_res))
     score = clf.score(X1, y1)

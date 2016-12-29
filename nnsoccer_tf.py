ea4f3dee091a5d2165e33801f65e1bf84a31615c
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-29 23:25:16 +0100

    Initial tf implementation

--- nnsoccer_tf.py
+++ nnsoccer_tf.py
@@ -174,7 +174,7 @@ class SoccerPredictorTF:
         for x in range(len(temp_games)):
             temp = []
             for y in range(len(res[x])):
-                temp.append(res[x][y] + res_svm[x][y])
+                temp.append(res[x][y]/2 + res_svm[x][y]/2)
             results[temp_games[x]] = temp
         return results
 

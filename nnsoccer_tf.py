05f3901736fce047049de2afb3a5b0985a84a99d
shiflux <artem.piazzi1@stud.unifi.it>
2016-12-29 21:46:43 +0100

    Initial tf implementation

--- nnsoccer_tf.py
+++ nnsoccer_tf.py
@@ -25,7 +25,7 @@ class SoccerPredictorTF:
         y0 += y1
         feature_columns = [tf.contrib.layers.real_valued_column("", dimension=len(settings.my_global_features))]
         self.classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                                        hidden_units=[10,20,10],
+                                                        hidden_units=[10],
                                                          n_classes=2 if trainingType == "golnogol" else 3)
 
         self.classifier.fit(x = np.array(x0), y = np.array(y0), steps=2000)

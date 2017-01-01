ecb3450acef87b55f8fa1d78909f84b704303158
shiflux <artem.piazzi1@stud.unifi.it>
2017-01-01 20:10:27 +0100

    Log implementation

--- nnsoccer_tf.py
+++ nnsoccer_tf.py
@@ -19,10 +19,8 @@ class SoccerPredictorTF:
         self.tf_session = tf.InteractiveSession()
         self.training_x = list()
         self.training_y = list()
-        self.my_input_fn()
 
-
-    def my_input_fn(self):
+    def createTrainingSet(self, trainingType=None):
         if os.path.exists("training_games_list.save"):
             f = open("training_games_list.save", "rb")
             x0 = pickle.load(f)
@@ -42,15 +40,12 @@ class SoccerPredictorTF:
             f.close()
         self.training_x = x0
         self.training_y = y0
-        return x0, y0
-
-    def createTrainingSet(self, trainingType=None):
         feature_columns = [tf.contrib.layers.real_valued_column("", dimension=len(settings.my_global_features))]
         self.classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                                          hidden_units=[len(settings.my_global_features)*2,len(settings.my_global_features)*4,len(settings.my_global_features)],
                                                          n_classes=2 if trainingType == "golnogol" else 3)
 
-        self.classifier.fit(x=np.array(self.training_x), y=np.array(self.training_y), max_steps=100000)
+        self.classifier.fit(x=np.array(self.training_x), y=np.array(self.training_y), max_steps=20000)
         self.clf = svm.SVC(kernel='linear', C=settings.C1, gamma=settings.gamma, probability=True)
         self.clf.fit(self.training_x, self.training_y)
 

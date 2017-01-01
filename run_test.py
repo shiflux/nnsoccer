a9fc58bd3fa88e8405d7fbffd634b946d1505f03
shiflux <artem.piazzi1@stud.unifi.it>
2017-01-01 20:36:51 +0100

    Log implementation

--- run_test.py
+++ run_test.py
@@ -62,10 +62,10 @@ if __name__ == "__main__":
 
     print ("Number of 0: " + str(len(list_of_0)) + " - accuracy: " + (str(numpy.mean(list_of_0)) if len(list_of_0) > 0 else "0"))
     for_log.append("Number of 0: " + str(len(list_of_0)) + " - accuracy: " + (str(numpy.mean(list_of_0)) if len(list_of_0) > 0 else "0"))
-    print ("Number of 1: " + str(len(list_of_1)) + " - accuracy: " + (str(numpy.mean(list_of_1)) if len(list_of_1 > 0) else "0"))
-    for_log.append("Number of 1: " + str(len(list_of_1)) + " - accuracy: " + (str(numpy.mean(list_of_1)) if len(list_of_1 > 0) else "0"))
+    print ("Number of 1: " + str(len(list_of_1)) + " - accuracy: " + (str(numpy.mean(list_of_1)) if len(list_of_1) > 0 else "0"))
+    for_log.append("Number of 1: " + str(len(list_of_1)) + " - accuracy: " + (str(numpy.mean(list_of_1)) if len(list_of_1) > 0 else "0"))
     if not args.type == "golnogol":
-        print ("Number of 2: " + str(len(list_of_2)) + " - accuracy: " + (str(numpy.mean(list_of_2)) if len(list_of_2 > 0) else "0"))
-        for_log.append("Number of 2: " + str(len(list_of_2)) + " - accuracy: " + (str(numpy.mean(list_of_2)) if len(list_of_2 > 0) else "0"))
+        print ("Number of 2: " + str(len(list_of_2)) + " - accuracy: " + (str(numpy.mean(list_of_2)) if len(list_of_2) > 0 else "0"))
+        for_log.append("Number of 2: " + str(len(list_of_2)) + " - accuracy: " + (str(numpy.mean(list_of_2)) if len(list_of_2) > 0 else "0"))
     if args.log and len(for_log) > 0:
         myPredictor.saveLog(for_log)

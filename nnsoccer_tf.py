b508311fe57074d964b0810f000c62f6569c4393
shiflux <artem.piazzi1@stud.unifi.it>
2017-01-04 19:18:15 +0100

    Log implementation

--- nnsoccer_tf.py
+++ nnsoccer_tf.py
@@ -220,6 +220,7 @@ class SoccerPredictorTF:
                     if inverted:
                         temp_prob = (predicted_prob_inverted[x][p] + predicted_prob_svm_inverted[x][p]) / 2
                         probs[-1] = (probs[-1]*2 + temp_prob)/3
+                        probs[-1] = temp_prob
                 max_prob = max(probs)
                 max_index = probs.index(max_prob)
                 probs.remove(max_prob)
